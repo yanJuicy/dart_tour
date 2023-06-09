a75ec63447ebd92ae817c6d75de9d87028c9f000
yanJuicy <hanjub3@gmail.com>
2023-06-09 19:39:36 +0900

    Fianl Variables

--- main.dart
+++ main.dart
@@ -14,12 +14,16 @@ void main() {
   //   name.
   // }
 
-
   // 1.3 Nullable Variables
-  String? nico = 'nico';
-  nico = null;
-  if (nico != null) {
-    nico.isNotEmpty;
-  }
-  nico?.isNotEmpty;
+  // String? nico = 'nico';
+  // nico = null;
+  // if (nico != null) {
+  //   nico.isNotEmpty;
+  // }
+  // nico?.isNotEmpty;
+
+
+  // 1.4 Final Variables
+  final name = 'nico';
+  final String name2 = 'nico';
 }
