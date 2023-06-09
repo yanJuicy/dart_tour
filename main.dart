e91f63ebbdc1a9d37105eee276e2a56739b705b7
yanJuicy <hanjub3@gmail.com>
2023-06-09 17:08:47 +0900

    Dynamic Type

--- main.dart
+++ main.dart
@@ -1,9 +1,17 @@
 void main() {
-  print('hello world');
+  // print('hello world');
 
   // The Var Keyword
-  var name = '니꼬';
-  name = 'nico';
-  
+  // var name = '니꼬';
+  // name = 'nico';
 
+
+  // Dynamic Type
+  dynamic name;
+  if (name is String) {
+    name.
+  }
+  if (name is int) {
+    name.
+  }
 }
