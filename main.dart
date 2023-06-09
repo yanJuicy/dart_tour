742eb6abd1bc553041be89a62e6e13e612c8d4dd
yanJuicy <hanjub3@gmail.com>
2023-06-09 18:19:31 +0900

    Nullable Variables

--- main.dart
+++ main.dart
@@ -5,13 +5,21 @@ void main() {
   // var name = '니꼬';
   // name = 'nico';
 
-
   // Dynamic Type
-  dynamic name;
-  if (name is String) {
-    name.
+  // dynamic name;
+  // if (name is String) {
+  //   name.
+  // }
+  // if (name is int) {
+  //   name.
+  // }
+
+
+  // 1.3 Nullable Variables
+  String? nico = 'nico';
+  nico = null;
+  if (nico != null) {
+    nico.isNotEmpty;
   }
-  if (name is int) {
-    name.
-  }
+  nico?.isNotEmpty;
 }
