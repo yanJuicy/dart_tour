a4efdcdd8eaf31291da01be3fe96eff3f1da82f2
yanJuicy <hanjub3@gmail.com>
2023-06-12 18:51:51 +0900

    Dart Class

--- main.dart
+++ main.dart
@@ -1,154 +1,171 @@
-void main() {
-  // print('hello world');
+// void main() {
+//   // print('hello world');
 
-  // The Var Keyword
-  // var name = '니꼬';
-  // name = 'nico';
+//   // The Var Keyword
+//   // var name = '니꼬';
+//   // name = 'nico';
 
-  // Dynamic Type
-  // dynamic name;
-  // if (name is String) {
-  //   name.
-  // }
-  // if (name is int) {
-  //   name.
-  // }
+//   // Dynamic Type
+//   // dynamic name;
+//   // if (name is String) {
+//   //   name.
+//   // }
+//   // if (name is int) {
+//   //   name.
+//   // }
 
-  // 1.3 Nullable Variables
-  // String? nico = 'nico';
-  // nico = null;
-  // if (nico != null) {
-  //   nico.isNotEmpty;
-  // }
-  // nico?.isNotEmpty;
+//   // 1.3 Nullable Variables
+//   // String? nico = 'nico';
+//   // nico = null;
+//   // if (nico != null) {
+//   //   nico.isNotEmpty;
+//   // }
+//   // nico?.isNotEmpty;
 
-  // final name = 'nico';
-  // final String name2 = 'nico';
+//   // final name = 'nico';
+//   // final String name2 = 'nico';
 
-  // 1.5 Late Variables
-  // late final String name;
-  // // do something, go to api
-  // name = '';
-  // print(name);
+//   // 1.5 Late Variables
+//   // late final String name;
+//   // // do something, go to api
+//   // name = '';
+//   // print(name);
 
-  // 1.6 Constant Variables
-  // const max_allowed_price = 120;
+//   // 1.6 Constant Variables
+//   // const max_allowed_price = 120;
 
-  // 2.1 Lists
-  // var giveMeFive = true;
-  // var numbers = [
-  //   1,
-  //   2,
-  //   3,
-  //   4,
-  //   if (giveMeFive) 5,
-  // ];
-  // numbers.add(1);
-  // print(numbers);
+//   // 2.1 Lists
+//   // var giveMeFive = true;
+//   // var numbers = [
+//   //   1,
+//   //   2,
+//   //   3,
+//   //   4,
+//   //   if (giveMeFive) 5,
+//   // ];
+//   // numbers.add(1);
+//   // print(numbers);
 
-  // 2.2 String Interpolation
-/*   var name = 'nico';
-  var age = 10;
-  var greeting = "hello $name I\'m ${age + 2}";
-  print(greeting); */
+//   // 2.2 String Interpolation
+// /*   var name = 'nico';
+//   var age = 10;
+//   var greeting = "hello $name I\'m ${age + 2}";
+//   print(greeting); */
 
-  // 2.3 Collection For
-  // var oldFriends = ['nico', 'lynn'];
-  // var newFriends = [
-  //   'lewis',
-  //   'ralph',
-  //   'darren',
-  //   for (var friend in oldFriends) "💝 $friend",
-  // ];
-  // print(newFriends);
+//   // 2.3 Collection For
+//   // var oldFriends = ['nico', 'lynn'];
+//   // var newFriends = [
+//   //   'lewis',
+//   //   'ralph',
+//   //   'darren',
+//   //   for (var friend in oldFriends) "💝 $friend",
+//   // ];
+//   // print(newFriends);
 
-  // 2.4 Maps
-  // var palyer = {
-  //   'name': 'nico',
-  //   'xp': 19.99,
-  //   'supperpower': false,
-  // };
-  // Map<int, bool> player2 = {
-  //   1: true,
-  //   2: false,
-  //   3: true,
-  // };
-  // Map<List<int>, bool> player = {
-  //   [1, 2, 3, 5]: true,
-  // };
-  // List<Map<String, Object>> players3 = [
-  //   {
-  //     'name': 'nico',
-  //     'xp': 199993,
-  //   },
-  //   {
-  //     'name': 'nico',
-  //     'xp': 199993,
-  //   },
-  //   {
-  //     'name': 'nico',
-  //     'xp': 199993,
-  //   },
-  // ];
+//   // 2.4 Maps
+//   // var palyer = {
+//   //   'name': 'nico',
+//   //   'xp': 19.99,
+//   //   'supperpower': false,
+//   // };
+//   // Map<int, bool> player2 = {
+//   //   1: true,
+//   //   2: false,
+//   //   3: true,
+//   // };
+//   // Map<List<int>, bool> player = {
+//   //   [1, 2, 3, 5]: true,
+//   // };
+//   // List<Map<String, Object>> players3 = [
+//   //   {
+//   //     'name': 'nico',
+//   //     'xp': 199993,
+//   //   },
+//   //   {
+//   //     'name': 'nico',
+//   //     'xp': 199993,
+//   //   },
+//   //   {
+//   //     'name': 'nico',
+//   //     'xp': 199993,
+//   //   },
+//   // ];
 
-  // 2.5 Sets
-  // var numbers = {1, 2, 3, 4};
-  // Set<int> numbers2 = {1, 2, 3, 4};
-  // numbers.add(1);
-  // numbers.add(1);
-  // numbers.add(1);
-  // print(numbers);
+//   // 2.5 Sets
+//   // var numbers = {1, 2, 3, 4};
+//   // Set<int> numbers2 = {1, 2, 3, 4};
+//   // numbers.add(1);
+//   // numbers.add(1);
+//   // numbers.add(1);
+//   // print(numbers);
 
-  // 3 Defining Function
-  // String sayHello(String potato) {
-  //   // call api
-  //   // do something
-  //   return "Hello $potato nice to meet you";
-  // }
-  // num plus(num a, num b) => a + b;
-  // print(sayHello('nico'));
+//   // 3 Defining Function
+//   // String sayHello(String potato) {
+//   //   // call api
+//   //   // do something
+//   //   return "Hello $potato nice to meet you";
+//   // }
+//   // num plus(num a, num b) => a + b;
+//   // print(sayHello('nico'));
 
-  // 3.1 Named Parameters
-  // String sayHello({
-  //   String name = 'anon',
-  //   int age = 0,
-  //   String country = 'wakanda',
-  // }) {
-  //   return "Hello $name, $age, $country";
-  // }
-  // String sayHello2({
-  //   required String name,
-  //   required int age,
-  //   required String country,
-  // }) {
-  //   return "Hello $name, $age, $country";
-  // }
-  // print(sayHello(
-  //   age: 12,
-  //   country: 'cuba',
-  // ));
+//   // 3.1 Named Parameters
+//   // String sayHello({
+//   //   String name = 'anon',
+//   //   int age = 0,
+//   //   String country = 'wakanda',
+//   // }) {
+//   //   return "Hello $name, $age, $country";
+//   // }
+//   // String sayHello2({
+//   //   required String name,
+//   //   required int age,
+//   //   required String country,
+//   // }) {
+//   //   return "Hello $name, $age, $country";
+//   // }
+//   // print(sayHello(
+//   //   age: 12,
+//   //   country: 'cuba',
+//   // ));
 
-  // 3.3 Optional Positional Parameters
-  // String sayHello(String name, int age, [String? country = 'cuba']) {
-  //   return "Hello $name, $age, $country";
-  // }
-  // print(sayHello('nico', 12));
+//   // 3.3 Optional Positional Parameters
+//   // String sayHello(String name, int age, [String? country = 'cuba']) {
+//   //   return "Hello $name, $age, $country";
+//   // }
+//   // print(sayHello('nico', 12));
 
-  // 3.4 QQ Operator
-  // String capitalizeName(String? name) => name?.toUpperCase() ?? 'ANNO';
-  // print(capitalizeName('nico'));
-  // print(capitalizeName(null));
-  // String? name;
-  // name ??= 'nico';
-  // name ??= 'another';
-  // print(name);
+//   // 3.4 QQ Operator
+//   // String capitalizeName(String? name) => name?.toUpperCase() ?? 'ANNO';
+//   // print(capitalizeName('nico'));
+//   // print(capitalizeName(null));
+//   // String? name;
+//   // name ??= 'nico';
+//   // name ??= 'another';
+//   // print(name);
 
-  // 3,5 Typedef
-  print(sayHi({"name": "nico"}));
+//   // 3,5 Typedef
+//   print(sayHi({"name": "nico"}));
+// }
+
+// typedef UserInfo = Map<String, String>;
+
+// String sayHi(UserInfo userInfo) {
+//   return "Hi ${userInfo['name']}";
+// }
+
+
+// 4 Class
+class Player {
+  final String name = 'nico';
+  int xp = 1500;
+
+  void sayHello() {
+    print("Hi my name is $name");
+  }
 }
 
-typedef UserInfo = Map<String, String>;
-
-String sayHi(UserInfo userInfo) {
-  return "Hi ${userInfo['name']}";
+void main() {
+  var player = Player();
+  player.xp = 2000;
+  player.sayHello();
 }
